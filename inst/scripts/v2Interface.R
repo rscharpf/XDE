f716042fb1e4ab7289cd4462ab706e0a6bb7330c
rscharpf@jhsph.edu <rscharpf@jhsph.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-08 01:44:18 +0000

    Update the seed in the R object after calling the C function
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/XDE@58898 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/v2Interface.R
+++ inst/scripts/v2Interface.R
@@ -5,7 +5,7 @@ params <- XDE:::Parameters(object=expressionSetList,
 			   clinicalCovariate="adenoVsquamous")
 
 dms <- dims(params)
-params2 <- startingValues(params,
+params2 <- XDE:::startingValues(params,
 			  expressionSetList,
 			  "adenoVsquamous")
 dms2 <- dims(params2)
@@ -13,18 +13,15 @@ all.equal(dms, dms2)
 a <- b <- matrix(NA, 100, 3)
 for(i in 1:50){
 	## could remove the seed argument -- haakon updates this automatically
-	trace(XDE:::rupdateANu, browser)
-	params.v2 <- XDE:::rupdateANu(object=params2,
-				      nTry=5L,
-				      epsilon=0.1,
-				      dryrun=FALSE)
-	params.v2 <- XDE:::rupdateBDDelta(object=expressionSetList,
-					  hyper.params=hyper.params,
-					  params=params.v2,
-					  nTry=5L,
-					  seed=as.integer(ceiling(runif(1,0,100))),
-					  epsilon=0.1,
-					  dryrun=FALSE)
+	##trace(XDE:::rupdateANu, browser)
+	params2 <- XDE:::rupdateANu(object=params2,
+				    nTry=5L,
+				    epsilon=0.1,
+				    dryrun=FALSE)
+	params2 <- XDE:::rupdateBDDelta(object=params2,
+					nTry=5L,
+					epsilon=0.1,
+					dryrun=FALSE)
 	a[i,] <- params.v2[["a"]]
 	b[i,] <- params.v2[["b"]]
 }
