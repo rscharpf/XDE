1e52ad342d5c04803df4b524169fe0945b5b606a
Rob Scharp <rscharpf@jhsph.edu>
2011-10-08 01:44:05 +0000

    update interface to C code in RUpdates.R using the Parameters class
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/XDE@58896 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/scripts/v2Interface.R
+++ inst/scripts/v2Interface.R
@@ -1,19 +1,21 @@
 library(XDE)
 data(expressionSetList)
-hyper.params <- XDE:::HyperParams(object=expressionSetList,
-				  clinicalCovariate="adenoVsquamous")
-## params.v2 <- XDE:::Params(hyper.params)  ## not working -- usually invalid results
-params.xde <- new("XdeParameter", esetList=expressionSetList,
-		  phenotypeLabel="adenoVsquamous", one.delta=FALSE)
-params.v2 <- as(params.xde, "Params")
+## some initial values
+params <- XDE:::Parameters(object=expressionSetList,
+			   clinicalCovariate="adenoVsquamous")
+
+dms <- dims(params)
+params2 <- startingValues(params,
+			  expressionSetList,
+			  "adenoVsquamous")
+dms2 <- dims(params2)
+all.equal(dms, dms2)
 a <- b <- matrix(NA, 100, 3)
 for(i in 1:50){
 	## could remove the seed argument -- haakon updates this automatically
-	params.v2 <- XDE:::rupdateANu(object=expressionSetList,
-				      hyper.params=hyper.params,
-				      params=params.v2,
+	trace(XDE:::rupdateANu, browser)
+	params.v2 <- XDE:::rupdateANu(object=params2,
 				      nTry=5L,
-				      seed=as.integer(ceiling(runif(1,0,100))),
 				      epsilon=0.1,
 				      dryrun=FALSE)
 	params.v2 <- XDE:::rupdateBDDelta(object=expressionSetList,
