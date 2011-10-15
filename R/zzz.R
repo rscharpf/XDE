2a931b6cfddf56dcb6aaae076f59f7bb7a648437
rscharpf@jhsph.edu <rscharpf@jhsph.edu@bc3139a8-67e5-0310-9ffc-ced21a209358>
2011-10-15 11:45:11 +0000

    Add .onUnload to zzz.R
    git-svn-id: https://hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/XDE@59273 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,4 +1,10 @@
+THISPKG <- "XDE"
+
 .onAttach <- function(libname, pkgname) {
 	version <- packageDescription("XDE", field="Version")
 	packageStartupMessage("Welcome to XDE version ", version)
+}
+
+.onUnload <- function( libpath ){
+	library.dynam.unload(THISPKG, libpath)
 }
