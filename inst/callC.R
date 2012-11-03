0aa6674a419940bf9d4aa95a52462e9860d73c7a
Rob Scharpf <rscharpf@jhsph.edu>
2012-11-03 00:29:03 -0400

    add inst/callC.R

---
+++ inst/callC.R
@@ -0,0 +1,35 @@
+library(Test)
+initializeParams()
+
+## add arguments for number genes, etc. so that these are not hardcoded
+##
+## have updated parameters written to files
+##
+## Can only pass 65 arguments
+## TODO
+##
+## - change initializeParams such that G, Q, psi (clinical
+##   parameters), etc. are inputs
+##
+## - have an option that selects which parameters are
+##   written to file
+##
+## - modify such that mcmc samples are appended to files according to
+##  previous option (see how sigma2 and phi are currently written to
+##  file, and adopt this for other parameters)
+##
+## - modify such that only selected iterations are appended to file
+##
+## - modify such that data is passed to the function and not simulated
+## - (or have an option that allows passing data)
+##
+## - many of the specific update types are already available with R wrappers
+##   (see R/Rupdates.R)
+##   -> initializeParams could be changed to only provide initial parameters
+##   -> using .C, we would need to specify the correct size for each parameter as input
+##      to initializeParams function
+##
+
+
+
+
